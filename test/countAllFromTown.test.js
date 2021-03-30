8c712932d530ca0e62a568ffe49f2af771fdb002
Athandile_Nkcaza <nkcazaathandile@gmail.com>
2021-03-31 01:49:01 +0200

    updated files

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -1,8 +1,14 @@
 describe('The allFromTown function', function(){
-    it('should return all the registration numbers in the string that is for that town(Stellies)', function(){
-     assert.equal('Stellies', allFromTown('CL 124', 'CL 345', 'CL 341', 'CL'));
+    it('should return all the registration numbers in the string that is for that town', function(){
+     assert.equal([], allFromTown('CL 124', 'CY 567', 'CL 345', 'CJ 456', 'CL 341'));
      
     });
+
 })
+    /* it('should return all the registration numbers in the string that is for Stellies', function(){
+        assert.equal('fromStellies', allFromTown( ['CL 124', 'CL 345', 'CL 341']));
+     });*/
+     
+
 
 // 'CL 124,CY 567,CL 345, CJ 456,CL 341, CL 124,CY 567,CL 345, CJ 456,CL 341, CL 124,CY 567,CL 345, CJ 456,CL 341'
